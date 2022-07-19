075efffbc16ad34f35722b034e70932924479909
Vallegit <valerio.akman@gmail.com>
2022-07-19 17:55:29 +0200

    encoding

--- upg2/upg2.cpp
+++ upg2/upg2.cpp
@@ -11,7 +11,7 @@ int main(){
     int antal = 0;
     int bestTot = 99*60*60;
 
-    // Loop l„ser in data, r„knar ut sluttid och j„mf”r mot b„sta tiden.
+    // Loop lÃ¤ser in data, rÃ¤knar ut sluttid och jÃ¤mfÃ¶r mot bÃ¤sta tiden.
     while(true){
         cout << "Startnummer? ";
         cin >> startNmr;
@@ -23,11 +23,11 @@ int main(){
         cout << "Sluttid? ";
         cin >> slutTimme >> slutMinut >> slutSekund;
 
-        // G”r om HH:MM:SS format till endast sekunder.
+        // GÃ¶r om HH:MM:SS format till endast sekunder.
         startTot = startTimme*60*60 + startMinut*60 + startSekund;
         slutTot = slutTimme*60*60 + slutMinut*60 + slutSekund;
 
-        // Tar h„nsyn till fall med tids”verg†ng vid midnatt.
+        // Tar hÃ¤nsyn till fall med tidsÃ¶vergÃ¥ng vid midnatt.
         if(startTot > slutTot){
           diff = slutTot - (startTot - 24*60*60);
         }
@@ -35,7 +35,7 @@ int main(){
           diff = slutTot - startTot;
         }
 
-        // J„mf”r om nuvarande startnummer har den b„sta tiden.
+        // JÃ¤mfÃ¶r om nuvarande startnummer har den bÃ¤sta tiden.
         if(bestTot > diff){
           vinnare = startNmr;
           bestTot = diff;
@@ -44,19 +44,19 @@ int main(){
         antal++;
     }
 
-    // G”r om sekunder till HH:MM:SS format.
+    // GÃ¶r om sekunder till HH:MM:SS format.
     bestTimme = bestTot/3600;
     bestMinut = (bestTot%3600)/60;
     bestSekund = bestTot % 60;
 
-    // Skriver ut Vinnare, tiden samt antalet t„vlande om det fanns t„vlande.
+    // Skriver ut Vinnare, tiden samt antalet tÃ¤vlande om det fanns tÃ¤vlande.
     if(antal <= 0) {
-      cout << "Inga T„vlande";
+      cout << "Inga TÃ¤vlande";
     }
     else {
-    cout << "Vinnare „r startnr: " << vinnare;
+    cout << "Vinnare Ã¤r startnr: " << vinnare;
     cout << "\nTim: " << bestTimme << " Min: " << bestMinut << " Sek: " << bestSekund;
-    cout << "\nAntal T„vlande: " << antal;
+    cout << "\nAntal TÃ¤vlande: " << antal;
     }
 
   return 0;
